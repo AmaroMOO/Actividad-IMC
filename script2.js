0a1e36fa075fbb0e320f93bccae02d8e0febf425
Amaro Morales <151141145+AmaroMOO@users.noreply.github.com>
2025-04-11 08:15:33 +0100

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btnCalcular").addEventListener("click", calcularIMC());
-
-function calcularIMC(){
-    let peso = document.getElementById("campoPeso").value;
-    let altura = document.getElementById("campoAltura").value;
-    console.log(peso);
-    console.log(altura);
-}
